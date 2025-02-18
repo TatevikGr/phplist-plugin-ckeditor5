6d4ad5d3da738bffcff7aaea99de7f9ffc73d74a
Tatevik <tatevikg1@gmail.com>
2025-02-18 21:03:29 +0400

    configs

--- plugins/CKEditor5Plugin.php
+++ plugins/CKEditor5Plugin.php
@@ -15,22 +15,115 @@ class CKEditor5Plugin extends phplistPlugin
     public $description = 'Provides the CKEditor5 for editing messages and templates.';
     public $documentationUrl = 'https://resources.phplist.com/plugin/ckeditor5';
     public $enabled = 1;
+    private $elEnabled;
+
+    public function __construct()
+    {
+        $this->elEnabled = defined('UPLOADIMAGES_DIR') && UPLOADIMAGES_DIR !== false;
+        $this->coderoot = dirname(__FILE__) . self::CODE_DIR;
+        $this->version = (is_file($f = $this->coderoot . self::VERSION_FILE))
+            ? file_get_contents($f)
+            : '';
+        $this->settings = array(
+            'ckeditor5_url' => array(
+                'value' => '//cdn.ckeditor.com/ckeditor5/34.0.0/classic/ckeditor.js',
+                'description' => 'URL of ckeditor.js',
+                'type' => 'text',
+                'allowempty' => 0,
+                'category' => 'CKEditor',
+            ),
+            'ckeditor_disallow' => array(
+                'description' => 'Disallow javascript',
+                'type' => 'boolean',
+                'value' => '1',
+                'allowempty' => true,
+                'category' => 'CKEditor',
+            ),
+        );
+
+        if (isset($_SESSION['ui']) && $_SESSION['ui'] == 'dressprow') {
+            $this->settings += [
+                'ckeditor_width' => [
+                    'value' => 600,
+                    'description' => 'Width in px of CKeditor Area',
+                    'type' => 'integer',
+                    'allowempty' => 0,
+                    'min' => 100,
+                    'max' => 800,
+                    'category' => 'CKEditor',
+                ],
+                'ckeditor_height' => [
+                    'value' => 600,
+                    'description' => 'Height in px of CKeditor Area',
+                    'type' => 'integer',
+                    'allowempty' => 0,
+                    'min' => 100,
+                    'max' => 800,
+                    'category' => 'CKEditor',
+                ],
+            ];
+        }
+
+        if ($this->elEnabled) {
+            $this->settings += array(
+                'kcfinder_path' => array(
+                    'value' => PLUGIN_ROOTDIR . self::CODE_DIR . 'elfinder',
+                    'description' => 'Path to ElFinder',
+                    'type' => 'text',
+                    'allowempty' => 0,
+                    'category' => 'CKEditor',
+                ),
+                'kcfinder_uploaddir' => array(
+                    'value' => '',
+                    'description' => 'File system path to the upload image directory. Usually leave this empty.',
+                    'type' => 'text',
+                    'allowempty' => 1,
+                    'category' => 'CKEditor',
+                ),
+                'kcfinder_image_directory' => array(
+                    'value' => 'image',
+                    'description' => 'Name of the image subdirectory of the file upload directory',
+                    'type' => 'text',
+                    'allowempty' => 0,
+                    'category' => 'CKEditor',
+                ),
+                'kcfinder_files_directory' => array(
+                    'value' => 'files',
+                    'description' => 'Name of the files subdirectory of the file upload directory',
+                    'type' => 'text',
+                    'allowempty' => 0,
+                    'category' => 'CKEditor',
+                ),
+            );
+        }
+        parent::__construct();
+    }
 
     public function editor($fieldName, $content): string
     {
         $width = getConfig('ckeditor_width') ?? 900;
         $height = getConfig('ckeditor_height') ?? 450;
-        $licenseKey = null; // getConfig('ckeditor_license_key');
+        $licenseKey = getConfig('ckeditor_license_key');
         $licenseKeyScript = "licenseKey: '$licenseKey'";
-        $editorUrl = getConfig('ckeditor_url') ? getConfig('ckeditor_url') : self::CDN;
+        $editorUrl = getConfig('ckeditor5_url') ? getConfig('ckeditor5_url') : self::CDN;
         $configVersion = $this->getCkeditorVersion($editorUrl) ?? '0.0.0';
         $cdnVersion = $this->getCkeditorVersion(self::CDN) ?? '0.0.0';
 
         if (version_compare($configVersion, $cdnVersion, '<')) {
             $editorUrl = self::CDN;
         }
+        $htmlSupport = '';
+        if (getConfig('ckeditor_disallow')) {
+            $htmlSupport = "htmlSupport: {
+                disallow: [
+                    { name: 'script' },
+                    { name: /.*/, attributes: { key: /^on.*/ } },
+                    { name: 'iframe' }
+                ]
+            }";
+        }
 
-        $script = $this->editorScript($fieldName, $width, $height, $licenseKeyScript, $editorUrl);
+        $script = $this->editorScript($fieldName, $width, $height, $licenseKeyScript, $editorUrl, $htmlSupport);
         $fieldName = htmlspecialchars($fieldName);
         $content = htmlspecialchars($content);
 
@@ -53,7 +146,7 @@ END;
         return "<textarea id=\"$fieldName\" name=\"$fieldName\">$content</textarea>";
     }
 
-    private function editorScript(string $fieldName, $width, $height, $licenseKeyScript, $editorUrl): string
+    private function editorScript(string $fieldName, $width, $height, $licenseKeyScript, $editorUrl, $htmlSupport): string
     {
         $pluginUrl = substr(PLUGIN_ROOTDIR, 0, 1) == '/' ? PLUGIN_ROOTDIR : $GLOBALS['pageroot'] . '/admin/' . PLUGIN_ROOTDIR;
 
@@ -79,6 +172,7 @@ document.addEventListener("DOMContentLoaded", function () {
     ClassicEditor
         .create(document.querySelector('textarea#$fieldName'), {
             $licenseKeyScript,
+            $htmlSupport,
             toolbar: [
                 'heading', 'undo', 'redo', '|', 
                 'bold', 'italic',  'bulletedList', 'numberedList', '|',
